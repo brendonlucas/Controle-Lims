e7765128cedd6d9dc5fe90b39e69d192a0a10808
brendonlucas <brendonplay007@gmail.com>
2019-05-27 10:57:19 -0300

    atualizações

--- sige/urls.py
+++ sige/urls.py
@@ -7,14 +7,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('equipamentos', views.exibir_equipamentos, name='equipamentos'),
-    path('adicionar_equipamento', views.add_equipamento, name='adicionar_equipamento'),
-    path('adicionar_usuario', views.add_user, name='adicionar_usuario'),
+    path('painel_admin/adicionar_equipamento', views.add_equipamento, name='adicionar_equipamento'),
+    path('painel_admin/adicionar_usuario', views.add_user, name='adicionar_usuario'),
     path('registros', views.exibir_registros, name='registros'),
     path('painel_admin', views.opcoes_admin, name='painel_admin'),
     path('logout', views.logout, name='logout'),
     path('login', views.login, name='login'),
     path('ajuda', views.exibir_ajuda, name='ajuda'),
-    path('editar_item', views.editar_item, name='editar_item'),
+    path('painel_admin/editar_item', views.editar_item, name='editar_item'),
 ]
 
 
